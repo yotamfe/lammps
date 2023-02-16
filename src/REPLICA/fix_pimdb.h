78b050841301e3d0422d1fa117a9039b539603c9
yotam <yotam.feldman@gmail.com>
2023-02-16 16:35:14 +0200

    arrg

--- src/REPLICA/fix_pimdb.h
+++ src/REPLICA/fix_pimdb.h
@@ -43,12 +43,11 @@ class FixPIMDB : public FixPIMD {
   double distance_squared_two_beads(const double* x1, int l1, const double* x2, int l2);
   double get_Enk(int m, int k);
   double set_Enk(int m, int k, double val);
-  void evaluate_connection_probabilities(const double* V, const double* V_backwards,
-                                         double* connection_probabilities);
-  void spring_force_last_bead(const double* connection_probabilities);
-  void spring_force_first_bead(const double* connection_probabilities);
-  void Evaluate_VBn(double* V, const int n);
-  void Evaluate_V_backwards(double* V_backwards);
+  void evaluate_connection_probabilities();
+  void spring_force_last_bead();
+  void spring_force_first_bead();
+  void Evaluate_VBn();
+  void Evaluate_V_backwards();
 
   int nbosons;
 
